88d4662435be8ba7facb367b0797cc12ea2a2726
Cleber Batista][ <cleberbatista99@hotmail.com>
2025-01-22 12:02:32 -0300

    Unindo variaveis e contantes

---
+++ variaveis e constantes/const.js
@@ -0,0 +1,12 @@
+// Variavel Constante é usada pra um valor que não muda 
+//não se pode mudar o valor de uma constante 
+
+const nome = "Cleber";
+
+
+//Atribuindo o valor de uma constante para outra constante 
+const uva = 10;
+const banada = 60;
+const valorDaComprar = uva * banada
+
+console.log(valorDaComprar)
---
+++ variaveis e constantes/index.js
@@ -0,0 +1,18 @@
+let nome1 = "Cleber";
+let nome2 =  "Nate"
+
+let nometeste; //Declarei a variavel 
+let nome ; //ela tem um valor mais esta indefinida
+
+// O uso do var é mais antigo , atualmente se usa LET o conceito é o mesmo
+// Não se pode criar variaves com palavras reservadas 
+// Variaveis precisam ter nomes significativos 
+// Não pode conter espaços ou traços 
+// Ultilizamos camelcase 
+// case sensitive 
+// não ultilizar var e sim let
+
+console.log(nome1,"nasceu em 1999");
+console.log("EM 2021 CLEBER conheceu" , nome2);
+console.log(nome2,"teve 14 filhos em 2025");
+console.log("O filho de NATE se chamava tito");
