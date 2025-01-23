77b6ecfd076ba3e79e4dec525c8f910434c858cc
Cleber Batista][ <cleberbatista99@hotmail.com>
2025-01-23 12:03:28 -0300

    Aplicando conceitos de ECMA SCRIPT ao meu codigo padronizando sintaxe

---
+++ let/var.js
@@ -0,0 +1,15 @@
+//ECMA SCRIPT é uma especificação de limguagem de programação que padroniza o Jav script , é uma 
+//linguagem de script baseada em padrões defini a sintaxe , a estrutura e os recursos do Java Script 
+
+ //difrença de var e let 
+ // usando LET não podemos REDECLARFAR o valor de uma Variavel
+ let nome = "Cleber";
+ let nome =  "teste";
+
+// usando VAR podemos redeclarar as variaveis quantas vezes forem necessarias
+ var nome = "teste1"
+ var nome = "donald trump"
+ var nome = "lula livre"
+
+ 
+ console.log(nome)
