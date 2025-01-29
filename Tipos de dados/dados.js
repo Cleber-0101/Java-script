9abff26b78bb3d419e8499365152bbbbd6336c0c
Cleber Batista][ <cleberbatista99@hotmail.com>
2025-01-29 11:17:03 -0300

    Estudo de operadores aritmeticos

--- Tipos de dados/dados.js
+++ Tipos de dados/dados.js
@@ -1,5 +1,4 @@
-//   TIPOS DE DADOS
-
+// TIPOS DE DADOS  em jAVA SCRIPT
 // String 
 // Number
 // Undefined
@@ -8,8 +7,13 @@
 const nome = "Cleber" // STRINGS
 const cidade = "São Paulo" // STRING
 const altura = "1.82" //STRING
+const maiorDeidade = false // boolean = true or false (operador logico)
 var nome1
 
+//Declarando uma variavel do tipo nula 
+let Pais = null
+
+//valores undefined não apontam pra local nenhum na memoria
 console.log(nome1)
 
 const numeroDaCasa = 225; //number
@@ -21,8 +25,18 @@ const aprovado = true // Boolean = true , false (Lógico)
 
 //usando typeOf dessa forma conseguimos pegar o tipo do dado e tambem o valor da variavel
 console.log(typeof aprovado , aprovado)
+console.log(typeof nome , nome)
+console.log(typeof numeroDaCasa , numeroDaCasa)
+console.log(typeof numeroDaCasa , numeroDoApartamento)
 
-//Descobrindo o TIPO e o VALOR da variavel
-//console.log(typeof carro , carro)
+const a = [1,2]
+const b = a;
+
+console.log(a, b)
+
+//push adiciona um valor novo no final da lista = array
+b.push(3)
+console.log(a ,b)
+
 
 
---
+++ operadores aritmeticos/aritmeticos.js
@@ -0,0 +1,37 @@
+// Aritimeticos 
+
+// soma , subtração , mulltiplacação e divisão
+//concatenação em programação é unir doid valores 
+
+//somar
+const number1 = 5 ;
+const number2 = 10;
+console.log(number1 + number2)
+
+
+//Subtração
+const n1 = 10;
+const n2 = 50;
+console.log(n1 - n2)
+
+//multiplicação
+const teste = 10;
+const teste1 = 50;
+console.log(n1 * n2)
+
+//Divisão 
+const div1 = 50;
+const div2 = 500;
+const divTotal = div1 /div2
+
+//typeof mostra tanto o tipo como o valor da variavel
+console.log(typeof teste , teste1 )
+
+console.log(typeof divTotal , divTotal)
+
+
+
+
+
+
+
